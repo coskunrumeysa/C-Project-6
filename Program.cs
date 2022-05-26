588fedbd0544030a427162f973946176ec6d6158
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-26 15:35:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Beni Yaratan Elbet Yolunu Gösterir -Şuara 78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int kk;
+            kk = 2;
+            int uk;
+            uk = 4;
+            int alan;
+            alan = kk * uk;
+            Console.WriteLine("Dikdörtgen Alan :" + " " + alan);
+            Console.Read();
+        }
+    }
+}
